509cbffc2af8622f418938ecf0f9fa428d2a8b45
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-11-09 14:15:29 +0530

    Submit pending request

--- app/store/sagas/submitPendingAxiosRequest.ts
+++ app/store/sagas/submitPendingAxiosRequest.ts
@@ -4,20 +4,24 @@ import * as fieldReportAction from 'app/store/actions/fieldReportAction';
 import * as dashboardActions from 'app/store/actions/dashboardAction';
 import { getDashboarsStatus } from 'app/services/dashboard';
 export default function* submitPendingAxiosRequest(action: any) {
-  //   console.log('Showing pending requests');
-  const events = action.events;
-  //   console.log('evets', events);
-  // map all requests
-  let promiseArray = events.map((data) => call(fieldReport, data));
-  const responseData = yield all(promiseArray);
-  //   console.log('server response', responseData);
-  for (var data of responseData) {
-    if (data.data.success) {
-      yield put(fieldReportAction.removeServerReportsById(data.data.id));
-      // field report reducer removefieldreportsbyid
-      yield put(fieldReportAction.removeFieldReportsById(data.data.id));
+  try {
+    const events = action.events;
+    //   console.log('evets', events);
+    // map all requests
+    // let promiseArray = events.map((data) => call(fieldReport, data));
+    // const responseData = yield all(promiseArray);
+    // //   console.log('server response', responseData);
+    for (var event of events) {
+      const response = yield call(fieldReport, event);
+      if (response.data.success) {
+        yield put(fieldReportAction.removeServerReportsById(response.data.id));
+        // field report reducer removefieldreportsbyid
+        yield put(fieldReportAction.removeFieldReportsById(response.data.id));
+      }
     }
+    const dashboard = yield call(getDashboarsStatus);
+    yield put(dashboardActions.onDashboardStatus(dashboard.data));
+  } catch (error) {
+    console.log(error);
   }
-  const dashboard = yield call(getDashboarsStatus);
-  yield put(dashboardActions.onDashboardStatus(dashboard.data));
 }
